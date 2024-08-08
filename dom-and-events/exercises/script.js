84fbe1f716c532096c34d40bd312492646c2cbf9
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-08-08 11:42:07 -0400

    Completed exercises/parts 1, 2, and 3

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,16 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
-}
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston, we have liftoff!';
+    });
 
+    missionAbort.addEventListener("mouseover", function( event) {
+       missionAbort.style.backgroundColor = "red"
+    });
+
+    missionAbort.addEventListener("mouseout", function( event) {
+        event.target.style.backgroundColor = "";
+    });
+}
 window.addEventListener("load", init);
