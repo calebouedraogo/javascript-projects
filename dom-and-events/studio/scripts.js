6f5b3052679b610f5313076289ed6430c3b3cb23
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-08-09 14:32:59 -0400

    Added Events Listener to the Land and Abort Mission buttons

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -5,12 +5,28 @@ window.addEventListener("load", function() {
     const flightStatus = document.getElementById("flightStatus");
     const shuttleBackground = document.getElementById("shuttleBackground");
     const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+    const landing = document.getElementById("landing");
+    const missionAbort = document.getElementById("missionAbort");
     takeOff.addEventListener("click", function(event) {
-        let confirmation =  confirm("Confirm that the shuttle is ready for takeoff");
+        let confirmation = confirm("Confirm that the shuttle is ready for takeoff");
         if (confirmation) {
             flightStatus.innerHTML = "Shuttle in flight";
             shuttleBackground.style.backgroundColor = "blue";
             spaceShuttleHeight.innerHTML = Number("10000");
         }
-    })
+    });
+    landing.addEventListener("click", function() {
+        alert("The shuttle is landing. Landing gear engaged");
+        flightStatus.innerHTML = "The shuttle has landed";
+        shuttleBackground.style.backgroundColor = "green";
+        spaceShuttleHeight.innerHTML = Number("0");
+    });
+    missionAbort.addEventListener("click", function() {
+        let confirmation = confirm("Confirm that you want to abort the mission");
+        if(confirmation) {
+            flightStatus.innerHTML = "Mission aborted";
+            shuttleBackground.style.backgroundColor = "green";
+            spaceShuttleHeight.innerHTML = Number("0");
+        }
+    });
 });
