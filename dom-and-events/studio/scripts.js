b4dfb8826af72a7e55541a707d1d8d63b4200aa7
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-08-09 12:14:12 -0400

    Added Event Listener to ask for confirmation when take off button clicked and changed the status if confirmed

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -1,2 +1,12 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener("load", function() {
+    const takeOff = document.getElementById("takeoff");
+    const flightStatus = document.getElementById("flightStatus");
+    takeOff.addEventListener("click", function(event) {
+        let confirmation =  confirm("Confirm that the shuttle is ready for takeoff");
+        if (confirmation) {
+            flightStatus.innerHTML = "Shuttle in flight";
+        }
+    })
+});
