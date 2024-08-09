dfefb5bf1ceaf4b03860195f8ee3dd12b39d62d4
CALEB OUEDRAOGO <calebouedraogo@calebs-mbp.mynetworksettings.com>
2024-08-09 12:34:31 -0400

    Added event listener to change the shuttle background color when in flight and updated the height to 10000 miles

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -3,10 +3,14 @@
 window.addEventListener("load", function() {
     const takeOff = document.getElementById("takeoff");
     const flightStatus = document.getElementById("flightStatus");
+    const shuttleBackground = document.getElementById("shuttleBackground");
+    const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
     takeOff.addEventListener("click", function(event) {
         let confirmation =  confirm("Confirm that the shuttle is ready for takeoff");
         if (confirmation) {
             flightStatus.innerHTML = "Shuttle in flight";
+            shuttleBackground.style.backgroundColor = "blue";
+            spaceShuttleHeight.innerHTML = Number("10000");
         }
     })
 });
